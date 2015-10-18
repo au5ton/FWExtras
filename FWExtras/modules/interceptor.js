780ad2bb7a7dee44f089715f222e62c464c1bd37
Austin Jackson <au5ton@icloud.com>
2015-10-18 01:27:51 -0500

    Fixed interceptor to work with options.js

--- FWExtras/modules/interceptor.js
+++ FWExtras/modules/interceptor.js
@@ -5,7 +5,21 @@
 *
 */
 
-$(document).on('FWExtrasOptionsLoaded',function(){
+var _globalOptions = {};
+
+chrome.storage.local.get({
+    chat_base: true,
+    nicknames_base: true,
+    interceptor_localassets: false,
+    mentions_pane: false,
+    mentions_highlighter: false,
+    blackjack_base: false,
+    loteria_suggestions: false,
+    loteria_bot: false
+}, function(items) {
+    _globalOptions = items;
+    console.log(_globalOptions);
+
 
     if(_globalOptions.chat_base === true) {
 
@@ -25,36 +39,33 @@ $(document).on('FWExtrasOptionsLoaded',function(){
     if(_globalOptions.interceptor_localassets === true) {
 
         //Redirects asset requests to local copies for speed
-        chrome.webRequest.onBeforeRequest.addListener(
-            function(details) {
-                if(details.type !== 'xmlhttprequest'){
-                    //console.log(details.type+'  ('+details.url+')');
-                    var requestedAsset = details.url.substring('http://108.197.28.233'.length);
-                    //console.log('requestedAsset:',requestedAsset)
-                    if(requestedAsset.indexOf('/loteria/') !== -1) {
-                        //likely a loteria image
-                        //console.log('/loteria/ file?');
-                        console.log('Intercepted '+requestedAsset+' to '+chrome.extension.getURL('../assets'+requestedAsset.substring('/img'.length)));
-                        return {redirectUrl: chrome.extension.getURL('../assets'+requestedAsset.substring('/img'.length))}
-                    }
-                    if(requestedAsset.indexOf('/cards/') !== -1) {
-                        //likely a blackjack card
-                        //console.log('/cards/ file?');
-                        //console.log(chrome.extension.getURL('../assets'+requestedAsset.substring('/img'.length)))
-                        console.log('Intercepted '+requestedAsset+' to '+chrome.extension.getURL('../assets'+requestedAsset.substring('/img'.length)));
-                        return {redirectUrl: chrome.extension.getURL('../assets'+requestedAsset.substring('/img'.length))}
-                    }
-                    /*if(requestedAsset.indexOf('/media/') !== -1) {
-                    //console.log('/media/ file?');
-                    console.log('Intercepted '+requestedAsset+' to '+chrome.extension.getURL('../assets'+requestedAsset));
-                    return {redirectUrl: chrome.extension.getURL('../assets'+requestedAsset)};
-                }*/
-            }
-            return {redirectUrl: details.url};
-        },
-        {urls: ["*://108.197.28.233/*"]},
-        ["blocking"]
-    );
+        chrome.webRequest.onBeforeRequest.addListener(function(details) {
+            if(details.type !== 'xmlhttprequest'){
+                //console.log(details.type+'  ('+details.url+')');
+                var requestedAsset = details.url.substring('http://108.197.28.233'.length);
+                //console.log('requestedAsset:',requestedAsset)
+                if(requestedAsset.indexOf('/loteria/') !== -1) {
+                    //likely a loteria image
+                    //console.log('/loteria/ file?');
+                    console.log('Intercepted '+requestedAsset+' to '+chrome.extension.getURL('../assets'+requestedAsset.substring('/img'.length)));
+                    return {redirectUrl: chrome.extension.getURL('../assets'+requestedAsset.substring('/img'.length))}
+                }
+                if(requestedAsset.indexOf('/cards/') !== -1) {
+                    //likely a blackjack card
+                    //console.log('/cards/ file?');
+                    //console.log(chrome.extension.getURL('../assets'+requestedAsset.substring('/img'.length)))
+                    console.log('Intercepted '+requestedAsset+' to '+chrome.extension.getURL('../assets'+requestedAsset.substring('/img'.length)));
+                    return {redirectUrl: chrome.extension.getURL('../assets'+requestedAsset.substring('/img'.length))}
+                }
+                /*if(requestedAsset.indexOf('/media/') !== -1) {
+                //console.log('/media/ file?');
+                console.log('Intercepted '+requestedAsset+' to '+chrome.extension.getURL('../assets'+requestedAsset));
+                return {redirectUrl: chrome.extension.getURL('../assets'+requestedAsset)};
+            }*/
+        }
+        return {redirectUrl: details.url};
+    }, {urls: ["*://108.197.28.233/*"]},["blocking"]);
 }
+
 
 });
